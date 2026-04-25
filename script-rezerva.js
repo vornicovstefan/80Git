0ac28a46754c557dd936acf5e3e22e0746e84118
vornicovstefan <vornicovstefan@gmail.com>
2026-04-25 13:19:58 +0300

    Cateva schimbari

---
+++ script-rezerva.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const params = new URLSearchParams(window.location.search);
+    const oras = params.get('oras');
+    const pret = params.get('pret');
+
+    const elementNume = document.getElementById('destinatie-nume');
+    const elementPret = document.getElementById('destinatie-pret');
+
+    if (oras) {
+        elementNume.innerText = oras;
+    } else {
+        elementNume.innerText = "Destinație nespecificată";
+    }
+
+    if (pret) {
+        elementPret.innerText = pret;
+    }
+
+   
+    const form = document.getElementById('confirmareForm');
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            
+            const numeDestinatie = oras || 'destinația aleasă';
+            alert('Rezervarea pentru ' + numeDestinatie + ' a fost înregistrată cu succes! Te vom contacta în curând.');
+            
+          
+            window.location.href = 'Home.html';
+        });
+    }
+});
